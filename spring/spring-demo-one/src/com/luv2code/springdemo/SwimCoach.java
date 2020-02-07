425ba820c14ef40556861663bda4811ec75aa9f6
Geoffrey Graddy <geoffrey.graddy@gtri.gatech.edu>
2020-02-06 19:02:45 -0500

    Spring Dependency Injection

--- spring/spring-demo-one/src/com/luv2code/springdemo/SwimCoach.java
+++ spring/spring-demo-one/src/com/luv2code/springdemo/SwimCoach.java
@@ -2,9 +2,20 @@ package com.luv2code.springdemo;
 
 public class SwimCoach implements Coach {
 
+    private FortuneService fortuneService;
+
+    public SwimCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Swim 20 Laps!";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
     }
 
 }
