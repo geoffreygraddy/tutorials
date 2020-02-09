d658dfa64b855e83a0ac97c8eb0a448408d66a85
Geoffrey Graddy <geoffrey.graddy@gtri.gatech.edu>
2020-02-09 13:05:24 -0500

    Form Tags and Data Binding with Checkboxes

--- spring/spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
+++ spring/spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
@@ -9,6 +9,7 @@ public class Student {
     private String country;
     private LinkedHashMap<String, String> countryOptions;
     private String favoriteLanguage;
+    private String[] operatingSystems;
 
     public Student() {
 
@@ -57,5 +58,13 @@ public class Student {
 
     public void setFavoriteLanguage(String favoriteLanguage) {
         this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public String[] getOperatingSystems() {
+        return operatingSystems;
+    }
+
+    public void setOperatingSystems(String[] operatingSystems) {
+        this.operatingSystems = operatingSystems;
     }
 }
