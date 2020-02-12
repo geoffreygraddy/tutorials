d4097d1dc806ecc6e81786d71633af104410263a
ggraddy3 <geoffrey.graddy@gtri.gatech.edu>
2020-02-12 12:33:51 -0500

    Completed Creating Custom Validaiton Rules

--- spring/spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
+++ spring/spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.luv2code.springdemo.mvc.validation.CourseCode;
+
 public class Customer {
 
     private String firstName;
@@ -21,6 +23,9 @@ public class Customer {
 
     @Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
     private String postalCode;
+
+    @CourseCode(value="TOPS", message="must start with TOPS")
+    private String courseCode;
 
     public String getFirstName() {
         return firstName;
@@ -54,5 +59,12 @@ public class Customer {
         this.postalCode = postalCode;
     }
 
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 }
 
