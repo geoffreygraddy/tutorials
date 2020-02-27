9d5c843bfbcb64522eee35e1096a8e8b70417ff6
ggraddy3 <geoffrey.graddy@gtri.gatech.edu>
2020-02-26 20:48:47 -0500

    Build Database Web App Part 2

--- web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,5 +1,10 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customer")
 public class CustomerController {
 
+    // need to inject the customer dao
+    @Autowired
+    private CustomerDAO customerDAO;
+
     @RequestMapping("/list")
     public String listCustomers(Model theModel) {
+
+        // get customers from the dao
+        List<Customer> theCustomers = customerDAO.getCustomers();
+
+        // add the customers to the model
+        theModel.addAttribute("customers", theCustomers);
 
         return "list-customers";
 
