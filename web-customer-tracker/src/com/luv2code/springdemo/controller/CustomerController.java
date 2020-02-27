01aae8c3487abfd3e7dda929aaec1539b31d0c28
ggraddy3 <geoffrey.graddy@gtri.gatech.edu>
2020-02-27 18:48:59 -0500

    Spring MVC and Hibernate Project - Part 3

--- web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -2,26 +2,27 @@ package com.luv2code.springdemo.controller;
 
 import java.util.List;
 
-import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
-    // need to inject the customer dao
+    // need to inject customer service
     @Autowired
-    private CustomerDAO customerDAO;
+    private CustomerService customerService;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model theModel) {
 
-        // get customers from the dao
-        List<Customer> theCustomers = customerDAO.getCustomers();
+        // get customers from the service
+        List<Customer> theCustomers = customerService.getCustomers();
 
         // add the customers to the model
         theModel.addAttribute("customers", theCustomers);
