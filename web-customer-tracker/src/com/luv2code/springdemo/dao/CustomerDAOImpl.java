fd129747acc5300a9dbfeee5578903bfdcbc061a
ggraddy3 <geoffrey.graddy@gtri.gatech.edu>
2020-02-27 20:10:43 -0500

    Spring MVC and Hibernate Project - Part 4

--- web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -8,7 +8,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class CustomerDAOImpl implements CustomerDAO {
@@ -23,14 +22,26 @@ public class CustomerDAOImpl implements CustomerDAO {
         // get the current hibernate session
         Session currentSession = sessionFactory.getCurrentSession();
 
-        // create a query
-        Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+        // create a query ... sort by last name
+        Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName",
+                                                                  Customer.class);
 
         // execute query and get result list
         List<Customer> customers = theQuery.getResultList();
 
         // return the results
         return customers;
+    }
+
+    @Override
+    public void saveCustomer(Customer theCustomer) {
+
+        // get current hibernate session
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        // save the customer ... finally LOL
+        currentSession.save(theCustomer);
+
     }
 
 }
